ddfc2fe7844bc8fd391425ebaad576657991aac6
Ömer MEMES <omer.memes20@gmail.com>
2024-06-20 19:45:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pure_vs_Impure.run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pure Vs. Impure Function/Pure_vs_Impure.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+/*
+    Saf Fonksiyon (Pure Function):
+        Aynı girdi ile her zaman aynı çıktıyı üretir.
+        Dış dünyadan herhangi bir yan etki (side effect) içermez, yani fonksiyon dışında herhangi bir 
+        durumu değiştirmez.
+        
+    
+    Saf Olmayan Fonksiyon (Impure Function):
+        Aynı girdi ile farklı çıktılar üretebilir.
+        Dış dünya ile etkileşime girer veya fonksiyon dışındaki durumu değiştirebilir. 
+*/
+internal class Pure_vs_Impure
+{
+    static List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+    static void Function1()
+    {
+
+        numbers.Add(15); // impure mutate gloabal variable
+    }
+
+    static void Function2(ref int number)
+    {
+        // sayinin uzerinde degisiklik yapildi
+        number++;  // impure mutate parameter
+        numbers.Add(number);
+    }
+
+    static void Function3()
+    {
+        // eklenen deger tahmin edilmesi zordur bunun icin saf bir fonksiyon sayilamaz    
+        numbers.Add(new Random().Next()); // impure interation with outside world
+    }
+
+    static List<int> Function4(List<int>lst, int number)
+    {
+        // pure function
+
+        var result = new List<int>(lst);
+        result.Add(number);
+        return result;
+    }
+    public static void run()
+    {
+        Action<List<int>> print = (List<int> nums) => Console.WriteLine(string.Join(", ", nums));
+
+        print(numbers);
+
+        Function1();
+
+        print(numbers);
+
+        int num = 15;
+        Function2(ref num);
+
+        print(numbers);
+
+        var result = Function4(numbers, 54);
+
+        print(numbers);
+        print(result);
+    }
+}
