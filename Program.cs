a5ad0a8167da457e3f48f484aa6a9943567f0c69
Ömer MEMES <omer.memes20@gmail.com>
2024-08-27 22:21:56 +0300

    all

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace LINQ
            
         static void Main(string[] args)
         {
-            Quantifiers.Any_Operation.Example.run();
+            Quantifiers.All_Operation.Example.run();
 
             Console.ReadKey();
         }
---
+++ Quantifiers/All Operation/Example.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Quantifiers.All_Operation
+{
+    public class Example
+    {
+        public static void run()
+        {
+            var emps = Repository.GetEmployeeInfo();
+
+            // Check if all employees have value for email.
+
+            var result1 = emps.All(emp => emp.Email is null);
+
+            if (result1)
+            {
+                Console.WriteLine("There are employee(s) not have email");
+            }
+            else
+            {
+                Console.WriteLine("all employees have email");
+            }
+
+            // Check if all employees have c# skill
+
+            var result = emps.All(emp => emp.Skills.Contains("C#"));
+            var result2 = emps.All(emp => emp.Skills.Any(skll => skll == "C#"));
+            if (result2)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
