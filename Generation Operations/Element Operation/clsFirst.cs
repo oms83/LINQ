371bf64e09b44f182d23db741bfe15499af421ff
Ömer MEMES <omer.memes20@gmail.com>
2024-08-30 18:49:12 +0300

    Last, LastOrDefault

--- Generation Operations/Element Operation/clsFirst.cs
+++ Generation Operations/Element Operation/clsFirst.cs
@@ -11,6 +11,7 @@ namespace LINQ.Generation_Operations.Element_Operation
         public static void run()
         {
             //Example01();
+            Example02();
         }
 
         static void Example01()
@@ -26,6 +27,17 @@ namespace LINQ.Generation_Operations.Element_Operation
             Console.WriteLine(question3);
 
         }
+        static void Example02()
+        {
+            var questions = QuestionBank.All;
 
+            var question1 = questions.Last();
+
+            Console.WriteLine(question1);
+
+            //var question2 = questions.Last(q => q.Title.Length == 0); // Invalid Operation Exception
+            var question3 = questions.LastOrDefault(q => q.Title.Length == 0);
+            Console.WriteLine(question3);
+        }
     }
 }
