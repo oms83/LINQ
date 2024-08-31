625c233d15231bfe6e54cec3770c54a807fc3bba
Ömer MEMES <omer.memes20@gmail.com>
2024-08-31 20:29:34 +0300

    Except, Inersection, Union

--- Sets Operations/Example.cs
+++ Sets Operations/Example.cs
@@ -11,10 +11,53 @@ namespace LINQ.Sets_Operations
         public static void run()
         {
             //Example01();
-            Example02();
+            //Example02();
+            //Example03();
+            //Example04();
+            Example05();
         }
 
-        //private static void Example02() 
+        // Union
+        private static void Example05()
+        {
+            var set1 = Repository.Meeting1.Participants;
+            var set2 = Repository.Meeting3.Participants;
+
+            set1.Print($"=========== Meeting 1 Participants ({set1.Count()})");
+            set2.Print($"=========== Meeting 2 Participants ({set2.Count()})");
+
+            var set3 = set1.Union(set2);
+
+            set3.Print("Set1 Union Set2");
+        }
+
+        // intersection
+        private static void Example04()
+        {
+            var set1 = Repository.Meeting1.Participants;
+            var set2 = Repository.Meeting3.Participants;
+
+            set1.Print($"=========== Meeting 1 Participants ({set1.Count()})");
+            set2.Print($"=========== Meeting 2 Participants ({set2.Count()})");
+
+            var set3 = set1.Intersect(set2);
+
+            set3.Print("Set1 Intersect Set2");
+        }
+
+        // exception
+        private static void Example03()
+        {
+            var set1 = Repository.Meeting1.Participants;
+            var set2 = Repository.Meeting3.Participants;
+
+            set1.Print($"=========== Meeting 1 Participants ({set1.Count()})");
+            set2.Print($"=========== Meeting 2 Participants ({set2.Count()})");
+
+            var set3 = set1.Except(set2);
+
+            set3.Print("Set1 Except Set2");
+        }
         private static void Example02()
         {
             var meetings = Repository.Meeting1.Participants.Concat(Repository.Meeting2.Participants);
