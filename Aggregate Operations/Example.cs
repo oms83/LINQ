ac0ecc1df528a3f69cc1744d6e2520fedf61cff3
Ömer MEMES <omer.memes20@gmail.com>
2024-08-31 11:31:15 +0300

    Aggregate Operation

---
+++ Aggregate Operations/Example.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Aggregate_Operations
+{
+    public class Example
+    {
+        public static void run()
+        {
+            //Example01();
+            //Example02();
+            //Example03();
+            Example04();
+        }
+
+        private static void Example04()
+        {
+            var questions = Concatenation.QuestionBank.All;
+            
+            var longestQuestionTitle = questions[0];
+
+            var title = questions
+                            .Aggregate(
+                            longestQuestionTitle, // seed
+                            (longest, next) => longest.Title.Length < next.Title.Length ? next : longest, // logic
+                            x => x // select result and return its
+                            );
+
+            Console.WriteLine(title);
+        }
+
+        private static void Example03()
+        {
+            var nums = new[] { 1, 2, 3, 4, 5 };
+
+            var result = nums.Aggregate(0, (sum, i) => sum + i);
+            Console.WriteLine(result); // 15
+
+            var result2 = nums.Aggregate(2, (sum, i) =>
+            {
+                Console.WriteLine(sum);
+                sum += i;
+                return sum;
+            });
+
+            Console.WriteLine(result2); // 17 seed starts from 2
+        }
+
+        private static void Example02()
+        {
+            var names = new[] { "omer", "ali", "osman", "yusuf", "musa" };
+
+            //********************************
+            var _result = string.Empty;
+
+            foreach (var iter in names)
+            {
+                _result += iter + ", ";
+            }
+            //********************************
+
+            var output = names.Aggregate((result, iter) => result += iter + ", ");
+
+            Console.WriteLine(output);
+        }
+
+        private static void Example01()
+        {
+            var names = new[] { "omer", "ali", "osman", "yusuf", "musa" };
+
+            var namesWithSeperater = string.Join(", ", names);
+            Console.WriteLine(namesWithSeperater);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace LINQ
            
         static void Main(string[] args)
         {
-            Concatenation.Example.run();
+            Aggregate_Operations.Example.run();
 
             Console.ReadKey();
         }
