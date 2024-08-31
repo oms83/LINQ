75b8ce95371e093ea52c2efb995974380cab2feb
Ömer MEMES <omer.memes20@gmail.com>
2024-08-31 22:29:47 +0300

    Build Expression Tree Using Api

--- Expression Trees/Example.cs
+++ Expression Trees/Example.cs
@@ -12,9 +12,28 @@ namespace LINQ.Expression_Trees
         public static void run()
         {
             //Example01();
-            Example02();
+            //Example02();
+            Example03();
         }
+        private static void Example03()
+        {
+            // (num)=>num%2==0
 
+
+            ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+            ConstantExpression zeroParam = Expression.Constant(0);
+            ConstantExpression twoParam = Expression.Constant(2);
+            BinaryExpression moduloBinaryExperssion = BinaryExpression.Modulo(numParam, twoParam);
+            BinaryExpression isEventExpression = BinaryExpression.Equal(moduloBinaryExperssion, zeroParam);
+
+            Expression<Func<int, bool>> IsEvenExpression = Expression.Lambda<Func<int, bool>>
+                                                            (isEventExpression, 
+                                                             new ParameterExpression[] { numParam});
+            Func<int, bool> IsEven = IsEvenExpression.Compile();
+
+            Console.WriteLine(IsEven(43));
+
+        }
         private static void Example02()
         {
             Expression<Func<int, bool>> isNegativeExpression = (num) => num < 0;
