19ab0620036a896e55bf47ef3aca1ea759f377c3
Ömer MEMES <omer.memes20@gmail.com>
2024-09-03 21:10:23 +0300

    Demo IEnumerable IQueryable

--- LINQ Anatomy/Examples.cs
+++ LINQ Anatomy/Examples.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LINQ.Extension_Method;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,35 @@ namespace LINQ.LINQ_Anatomy
     {
         public static void run()
         {
-            DemoFluentAPI();
+            //DemoFluentAPI();
+            DemoIEnumerableIQueryable();
         }
+        private static void DemoIEnumerableIQueryable()
+        {
+            // LINQ to Objects
+            // which mostly just does literally what it is told;
+            // if you sort then pages, then it sorts then pages;
+            // if you page then sort, then it pages then sorts;
 
+            var deck = new Deck();
+
+            var queryIEnumerable = deck.Shuffle()
+                .Where(x => x.Value < 5).Skip(5).OrderBy(x => x.Value)
+                .ThenByDescending(x => x.Suite).Take(5).AsEnumerable();
+
+         
+
+            // i.e LINQ to SQL
+            // Query is begin composed (Experssion Tree).
+            // When Execute Provider inspect your query tree.
+            // build the most suitable implementation possoble. 
+
+            var queryIQueryable = deck.Shuffle()
+               .Where(x => x.Value < 5).Skip(5).OrderBy(x => x.Value)
+               .ThenByDescending(x => x.Suite).Take(5).AsQueryable();
+
+
+        }
         private static void DemoFluentAPI()
         {
             var deck = new Deck();
