360ec48e45cca26cf8a05838f540c1f588f6fafd
Ömer MEMES <omer.memes20@gmail.com>
2024-09-04 12:41:35 +0300

    Demo Execution Order

--- LINQ Anatomy/Examples.cs
+++ LINQ Anatomy/Examples.cs
@@ -12,8 +12,66 @@ namespace LINQ.LINQ_Anatomy
         public static void run()
         {
             //DemoFluentAPI();
-            DemoIEnumerableIQueryable();
+            //DemoIEnumerableIQueryable();
+            DemoExecutionOrder();
         }
+
+        private static void DemoExecutionOrder()
+        {
+            // Left to Right (All Expression in C# are executed Left to Right
+
+
+            var numbers = new int[] { 8, 2, 3, 4, 1, 6, 5, 12, 9 };
+
+            var query = numbers.Where(x =>
+            {
+                Console.WriteLine($"Where({x} > 5) => {x > 5}");
+                return x > 5;
+            }).
+            Select(x =>
+            {
+                Console.WriteLine($"\tSelect({x} X {x}) => {x * x}");
+                return x * x;
+            }).
+            Where(x =>
+            {
+                var result = x % 6 == 0;
+                Console.WriteLine($"\t\tWhere({x} % 6) == 0 => {result}");
+                if (result)
+                {
+                    Console.WriteLine($"\t\t\tTake: {x}");
+                }
+
+                return x % 6 == 0;
+            }).
+            Take(2);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(query);
+            }
+
+            /*
+                Where(8 > 5) => True
+                        Select(8 X 8) => 64
+                                Where(64 % 6) == 0 => False
+                Where(2 > 5) => False
+                Where(3 > 5) => False
+                Where(4 > 5) => False
+                Where(1 > 5) => False
+                Where(6 > 5) => True
+                        Select(6 X 6) => 36
+                                Where(36 % 6) == 0 => True
+                                        Take: 36
+                System.Linq.Enumerable+<TakeIterator>d__25`1[System.Int32]
+                Where(5 > 5) => False
+                Where(12 > 5) => True
+                        Select(12 X 12) => 144
+                                Where(144 % 6) == 0 => True
+                                        Take: 144
+             */
+        }
+
         private static void DemoIEnumerableIQueryable()
         {
             // LINQ to Objects
